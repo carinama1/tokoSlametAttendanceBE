132374116cce0e837a4100a13c6fbe742d9e7bca
carinama1 <jarpani30@gmail.com>
2020-11-13 13:14:23 +0700

    add regular update

--- controllers/employees/index.js
+++ controllers/employees/index.js
@@ -1,5 +1,6 @@
 const { employees } = require("../../dbConfig");
 
+// /api/v1/employees/get
 exports.getEmployees = (req, res) => {
   // const item = { name: "tahu", payday: 20, phone: "081233476375" };
   // employees.insert(item);
@@ -8,6 +9,24 @@ exports.getEmployees = (req, res) => {
   });
 };
 
+// /api/v1/employees/add
 exports.insertEmployee = (req, res) => {
-  res.send("Insert Employees");
+  const { name, payday, phone, role = "pegawai" } = req.body;
+
+  employees.find({ name }).then((doc) => {
+    if (doc.length > 0) {
+      res.status(409).send(`${name} sudah terdaftar`);
+    } else {
+      const created = new Date();
+      employees
+        .insert({ name, payday, phone, role, created })
+        .then((doc) => {
+          res.status(201).send(`${name} berhasil didaftarkan`);
+        })
+        .catch((err) => {
+          res.status(500).send("something went wrong");
+          console.log(err);
+        });
+    }
+  });
 };
--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 const express = require("express");
 const cors = require("cors");
+const schedule = require("node-schedule");
 const port = process.env.PORT || 4009;
+
+let test = 1;
 
 const app = express();
 
 app.use(cors({ origin: "*" }));
+app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 const EmployeeRouter = require("./routes/employees");
 
-app.use("/employees", EmployeeRouter);
+app.use("/time", (req, res) => {
+  const today = new Date();
+  const date = today.getDate();
+  const month = today.getMonth();
+  const year = today.getFullYear();
+  const hour = today.getHours();
+  const minutes = today.getMinutes();
+  const day = today.getDay();
+
+  res.send({ date, month, year, day, hour, minutes });
+});
+
+const execute = schedule.scheduleJob({ second: 20 }, () => {
+  console.log("asd");
+  test += 1;
+});
+
+app.use("/api/v1/employees", EmployeeRouter);
 app.use("/", (req, res) => {
-  res.send({ message: "hello" });
+  res.send({ message: test });
 });
 
 app.listen(port, () => {
